0246760a5898e56c5ebf36fe90c178806f385020
Carlos Barrios <carlosbarriosf6@gmail.com>
2024-02-29 17:34:45 -0300

    removed hardcoded keywords and added catch statements

--- script.js
+++ script.js
@@ -1,37 +1,41 @@
 const apiEndpoint = "https://my-json-server.typicode.com/carlosbarriosf/fake-products-db/products"
 
 async function getProducts (api) {
-    const res = await fetch(api);
-    const data = await res.json();
-    return data
+    try {
+        const res = await fetch(api);
+        const data = await res.json();
+        return data
+    }
+    catch(err) {
+        console.log(err)
+    }
 }
 
 const mainElement = document.querySelector('main');
 const productContainer = document.querySelector('[data-products-container]');
 
 async function populateHtml(api) {
-
-
-    // const newDiv = document.createElement('div')
-
-    const productArray = await getProducts(api);
-
-    productArray.forEach(item => {
-        const productTemplate = document.querySelector('[data-product-template]');
-        const product = productTemplate.content.cloneNode(true).children[0];
-        const productName = product.querySelector('[data-product-name]');
-        
-        productName.innerText = item.name;
-
-
-        const productPrice = product.querySelector('[data-product-price]');
-        productPrice.innerText = `$${item.price}`;
-
-        // newDiv.appendChild(product);
-        productContainer.appendChild(product)
-    })
-
-    // productContainer.innerHTML += newDiv.innerHTML
+    try {
+        const productArray = await getProducts(api);
+    
+        productArray.forEach(item => {
+            const productTemplate = document.querySelector('[data-product-template]');
+            const product = productTemplate.content.cloneNode(true).children[0];
+            const productName = product.querySelector('[data-product-name]');
+            
+            productName.innerText = item.name;
+    
+    
+            const productPrice = product.querySelector('[data-product-price]');
+            productPrice.innerText = `$${item.price}`;
+    
+            productContainer.appendChild(product)
+        })
+    }
+    catch (err) {
+        console.log(err)
+        createErrorMessage('No se pudo conectar con el servidor')
+    }
 }
 
 document.addEventListener('DOMContentLoaded', () => {
@@ -41,17 +45,34 @@ document.addEventListener('DOMContentLoaded', () => {
 const searchButton = document.querySelector('[data-search-button]');
 const searchInput = document.getElementById('search');
 
-const keyWords = ['computadora', 'notebook', 'aire acondicionado', 'heladera', 'cocina',
-'lavarropas', 'secarropas', 'pava electrica', 'cafetera', 'licuadora'];
+
+
+let keyWords;
+
+const generateKeyWords = async () => {
+    try {
+        const products = await getProducts(apiEndpoint);
+        keyWordsArray = products.map(product => product.category);
+        keyWords = [... new Set(keyWordsArray)]
+        return keyWords
+    }
+    catch (err) {
+        console.log(err)
+    }
+} 
+
+generateKeyWords();
+
+
 
 searchButton.addEventListener('click', () => {
     const searchValue = searchInput.value.toLowerCase();
 
-    // let i = 0;
     let keyWordFound = false;
 
-    //I define i with the var keyword in the loop so it creates a closure
+    // I define i with the var keyword in the loop so it creates a closure
     //  that I can use later in the code
+
     for(var i = 0; i< keyWords.length; i++) {
         keyWordFound = keyWords[i].includes(searchValue);
         if (keyWordFound) break
@@ -60,25 +81,11 @@ searchButton.addEventListener('click', () => {
     let keyWord;
     if (keyWordFound) {
         keyWord = keyWords[i];
-        if(keyWord === 'computadora') {
-            keyWord = 'notebook';
-        }
         const apiToRequest = `${apiEndpoint}?category=${keyWord}`;
         clearTable()
-        
         populateHtml(apiToRequest);
-    
     } else {
-        // console.log('no se encontro nada')
-        const newTableRow = document.createElement('tr');
-        newTableRow.setAttribute('data-product', '')
-        const newTableData = document.createElement('td');
-        const emptyTableData = document.createElement('td');
-        newTableData.innerText = 'No se encontraron productos';
-        clearTable();
-        newTableRow.appendChild(newTableData);
-        newTableRow.appendChild(emptyTableData);
-        productContainer.appendChild(newTableRow);
+        createErrorMessage('No se encontraron productos')
     }
     
     searchInput.value = '';
@@ -92,4 +99,14 @@ function clearTable() {
     })
 }
 
-
+function createErrorMessage(message) {
+    const newTableRow = document.createElement('tr');
+        newTableRow.setAttribute('data-product', '')
+        const newTableData = document.createElement('td');
+        const emptyTableData = document.createElement('td');
+        newTableData.innerText = message;
+        clearTable();
+        newTableRow.appendChild(newTableData);
+        newTableRow.appendChild(emptyTableData);
+        productContainer.appendChild(newTableRow);
+}
